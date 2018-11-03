d0fc956ca9b9cab8c80d239ca0e048a42f94767f
Spyros <spyros.2@hotmail.com>
2018-11-03 11:55:42 +0100

    Initial commit

---
+++ buzzerTest/buzzerTest.ino
@@ -0,0 +1,26 @@
+/* Arduino tutorial - Buzzer / Piezo Speaker
+   More info and circuit: http://www.ardumotive.com/how-to-use-a-buzzer-en.html
+   Dev: Michalis Vasilakis // Date: 9/6/2015 // www.ardumotive.com */
+
+
+const int buzzer = 6; //buzzer to arduino pin 9
+byte state = 0;
+
+void setup(){
+ 
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+
+}
+
+void loop(){
+  if (state == 0){
+    tone(buzzer, 3124); // Send 1KHz sound signal...
+    state = 1;
+  }
+//  delay(1000);        // ...for 1 sec
+  if (state == 1){
+    noTone(buzzer); // Send 1KHz sound signal...
+    state = 0;
+  }
+  
+}
